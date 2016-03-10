f1dbbe411f0661f47d8c51452cae8ec30a6189c2
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-10 13:06:18 -0800

    updated node methods (rm get/set) updated stack pop

--- test_stack.py
+++ test_stack.py
@@ -10,39 +10,6 @@ def test_Node():
     assert isinstance(Node(data[0]), Node)
 
 
-def test_set_data():
-    """Test node module set data method."""
-    from stack import Node
-    test_node = Node(data[0])
-    test_node.set_data(data[1])
-    assert test_node.data == data[1]
-
-
-def test_set_next():
-    """Test node module set next method."""
-    from stack import Node
-    test_node1 = Node(data[0])
-    test_node2 = Node(data[1])
-    test_node1.set_next(test_node2)
-    assert test_node1.next_node == test_node2
-
-
-def test_get_data():
-    """Test node module get data method."""
-    from stack import Node
-    test_node = Node(data[0])
-    assert test_node.get_data() == data[0]
-
-
-def test_get_next_node():
-    """Test node module get next method."""
-    from stack import Node
-    test_node1 = Node(data[0])
-    test_node2 = Node(data[1])
-    test_node1.set_next(test_node2)
-    assert test_node1.get_next_node() == test_node2
-
-
 def test_Stack():
     """Test for stack init method."""
     from stack import Stack
@@ -63,11 +30,11 @@ def test_pop():
     from stack import Stack
     stack = Stack()
     stack.push(data[0])
-    assert stack.pop().data == data[0]
+    assert stack.pop() == data[0]
 
 
 def test_pop_empty():
     """Test stack pop method."""
     from stack import Stack
     stack = Stack()
-    assert stack.pop() is None
+    assert stack.pop() == 'empty list'
