ed9deabee9f47a4b865b922a2cb5fdb89128211a
David Flegal <flegal.david@gmail.com>
2016-04-20 15:18:21 -0700

    reconfigured test.

--- src/test_merge_sort.py
+++ src/test_merge_sort.py
@@ -5,5 +5,5 @@ def test_merge_sort():
     """Assert that it works for tox."""
     from merge_sort import merge_sort
     merge_return_val = [56, 93, 7, 11]
-    merge_sort(insertion_return_val)
+    merge_sort(merge_return_val)
     assert merge_return_val == [7, 11, 56, 93]
